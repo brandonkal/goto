3992400e44f3c274ffd55f78d92f690380c3464f
Brandon Kalinowski <4714862+brandonkal@users.noreply.github.com>
2019-12-25 00:29:52 +0000

    test ofc

--- goto/handler.js
+++ goto/handler.js
@@ -4,7 +4,7 @@ module.exports = (event, context) => {
     let redirect;
 
     if(event.path == "/home") {
-        redirect = "https://www.alexellis.io/";
+        redirect = "https://brandonkalinows.com/";
     } else if(event.path == "/sponsors" || event.path == "/insiders") {
         redirect = "https://github.com/users/alexellis/sponsorship";
     } else if(event.path == "/inlets") {
