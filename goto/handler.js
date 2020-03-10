6fc9f6b9b3d2e6a8408c3263e18f64d45fb00157
Brandon Kalinowski <brandon@brandonkalinowski.com>
2020-03-10 07:04:48 +0000

    formatting

--- goto/handler.js
+++ goto/handler.js
@@ -1,29 +1,27 @@
-"use strict"
+'use strict'
 
 module.exports = (event, context) => {
-    let redirect;
+	let redirect
 
-    if(event.path == "/home") {
-        redirect = "https://brandonkalinowski.com/";
-    } else if(event.path == "/inlets") {
-        redirect = "https://inlets.dev";
-    } else if(event.path == "/k3sup") {
-        redirect = "https://k3sup.dev";
-    } else if(event.path == "/github") {
-        redirect = "https://github.com/brandonkal";
-    } else if(event.path == "/git") {
-        redirect = "https://github.com/brandonkal";
-    }
+	if (event.path == '/home') {
+		redirect = 'https://brandonkalinowski.com/'
+	} else if (event.path == '/inlets') {
+		redirect = 'https://inlets.dev'
+	} else if (event.path == '/k3sup') {
+		redirect = 'https://k3sup.dev'
+	} else if (event.path == '/github') {
+		redirect = 'https://github.com/brandonkal'
+	} else if (event.path == '/git') {
+		redirect = 'https://github.com/brandonkal'
+	}
 
-    process.stderr.write(event.path)
+	process.stderr.write(event.path)
 
-    if(!redirect) {
-        return context
-            .status(400)
-            .fail("Unknown short URL");
-    }
-    context
-        .status(302)
-        .headers({"location": redirect})
-        .succeed();
+	if (!redirect) {
+		return context.status(400).fail('Unknown short URL')
+	}
+	context
+		.status(302)
+		.headers({ location: redirect })
+		.succeed()
 }
